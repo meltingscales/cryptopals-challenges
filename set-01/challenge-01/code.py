0382d3fce3c723de5e10f63e1067e5cd279fefe2
Henry Post <henryfbp@gmail.com>
2020-10-27 17:26:37 -0500

    start parsing triplets of base64

--- set-01/challenge-01/code.py
+++ set-01/challenge-01/code.py
@@ -1,6 +1,21 @@
-# import bytes
+# see https://en.wikipedia.org/wiki/Base64
+
+
+def print_bits(i:int)->None:
+    pass
+
+from typing import List
 
 with open('input', 'rb') as f:
 
+    data: List[int] = []
+
     for byte in f.read():
-        print(byte)
+        data.append(byte)
+    
+    padCount = len(data)%3
+
+    print(f'pad {padCount} times')
+
+    for i in range(0, len(data), 3):
+        print(i)
