d62f2919a876282ee648bc2e8cc3e538b6bc99d5
Henry Post <HenryFBP@gmail.com>
2020-11-06 22:55:25 -0600

    read files in rust

--- rs-cryptopals/src/main.rs
+++ rs-cryptopals/src/main.rs
@@ -1,3 +1,22 @@
+use std::{fs::File, io::Read, path::Path};
+
+fn read_datafile(filepath: String) -> String {
+    let mut f: File = match File::open(filepath.clone()) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening file '{:?}': {:?}", filepath, error),
+    };
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+
+    return contents;
+}
+
 fn main() {
+    let filepath = "../set-01/challenge-01/input";
+
     println!("Hello, world!");
+
+    let data:String=read_datafile(filepath.to_string());
+    print!("{}", data);
 }
