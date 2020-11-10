8727b23ec875ce1382c916855fa7a9ba84756bee
Henry Post <henryfbp@gmail.com>
2020-11-09 19:47:31 -0600

    improve bitstring fn

--- rs-cryptopals/src/bitstring.rs
+++ rs-cryptopals/src/bitstring.rs
@@ -1,12 +1,29 @@
-pub fn to_bitstring(num: u32, width: u32) -> String
-{
+pub fn to_bitstring_4bytes(num: u32, _spacing: Option<u32>) -> String {
+    return to_bitstring(num, Some(32), _spacing);
+}
+
+pub fn to_bitstring_byte(num: u32) -> String {
+    return to_bitstring(num, Some(8), None);
+}
+
+pub fn to_bitstring(num: u32, _width: Option<u32>, _spacing: Option<u32>) -> String {
+    let width = _width.unwrap_or(32);
+    let spacing = _spacing.unwrap_or(8);
+
     let mut n = num;
 
     let mut s = "".to_string();
-    for _ in 0..width {
+    for i in 0..width {
         let bit = n & 1;
 
         s = format!("{}{}", bit.to_string(), s);
+
+        // time to put a space. We're appending backwards so...
+        if (((width - i) - 1) % spacing) == 0 {
+            if i != 0 && i != width - 1 {
+                s = format!("{}{}", " ", s);
+            }
+        }
 
         n = n >> 1;
     }
